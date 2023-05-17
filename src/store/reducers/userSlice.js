e806ca593a4f75962c660e014b6adef4d7274582
jame windows10 <thanakornn.ch@gmail.com>
2023-05-18 06:36:32 +0700

    add store : user

---
+++ src/store/reducers/userSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    name: "",
+  },
+  reducers: {
+    setUserName: (state, action) => {
+      state.name = action.payload;
+    },
+  },
+});
+
+export const { setUserName } = userSlice.actions;
+
+export default userSlice.reducer;
